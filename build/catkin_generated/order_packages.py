0375124beb7b521a0965224786b421de49bea43f
dahlmill <dahlmill@qq.com>
2020-03-19 14:28:43 +0800

    0318

--- build/catkin_generated/order_packages.py
+++
@@ -1,5 +0,0 @@
-# generated from catkin/cmake/template/order_packages.context.py.in
-source_root_dir = "/home/dm/catkin_ws/src"
-whitelisted_packages = "".split(';') if "" != "" else []
-blacklisted_packages = "".split(';') if "" != "" else []
-underlay_workspaces = "/home/dm/catkin_ws/devel;/opt/ros/kinetic".split(';') if "/home/dm/catkin_ws/devel;/opt/ros/kinetic" != "" else []
