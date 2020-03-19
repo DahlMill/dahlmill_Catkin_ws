9754c41c00508a2918368c487f16ca010e9c4530
dahlmill <dahlmill@qq.com>
2020-03-19 14:29:52 +0800

    0318

--- build/slam_path/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/dm/catkin_ws/src/slam_path
-# Build directory: /home/dm/catkin_ws/build/slam_path
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
